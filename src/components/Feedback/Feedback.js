84f2bb49486e227845f00f4f0022d0c0a58ff45b
Zoriana-Burian <64468262+Zoriana-Burian@users.noreply.github.com>
2021-01-20 20:28:11 +0200

    s

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>Good</button>
+                <button>Neutral</button>
+                <button>Bad</button>
+            </div>
+        );
+    }
+};
